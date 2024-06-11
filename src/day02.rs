59929b77513769233de75b573f8722e11abf80a0
matheus.rossetti <matheus.rossetti@db1.com.br>
2024-06-11 16:13:34 -0300

    day02: part2

--- src/day02.rs
+++ src/day02.rs
@@ -4,49 +4,93 @@ pub fn part_1() -> u32 {
     let red = 12;
     let blue = 14;
     let green = 13;
+
     let mut sum: u32 = 0;
 
     for line in buffer.lines() {
-        let split = line.split(':').collect::<Vec<&str>>();
+        let (game_str, cube_str) = line.split_once(':').unwrap();
 
         let mut is_possible = true;
 
-        let game = split
-            .get(0)
-            .unwrap()
-            .replace("Game ", "")
-            .parse::<u32>()
-            .unwrap();
+        let game: u32 = game_str.replace("Game ", "").parse().unwrap();
 
-        let cube_sets = split.get(1).unwrap().split(';').collect::<Vec<&str>>();
+        for set in cube_str.split(';') {
+            for color in set.split(',') {
+                let (count, color_name) = color
+                    .trim()
+                    .split_at(color.trim().find(|x: char| x.is_whitespace()).unwrap_or(0));
 
-        for set in cube_sets {
-            let sets = set.split(',').collect::<Vec<&str>>();
+                let count: u32 = count.parse().unwrap();
 
-            for color in sets {
-                if color.contains("red") {
-                    let count = color.replace("red", "").trim().parse::<u8>().unwrap();
-                    if count > red {
-                        is_possible = false;
+                match color_name.trim() {
+                    "red" => {
+                        if count > red {
+                            is_possible = false;
+                        }
                     }
-                }
-                if color.contains("blue") {
-                    let count = color.replace("blue", "").trim().parse::<u8>().unwrap();
-                    if count > blue {
-                        is_possible = false;
+                    "blue" => {
+                        if count > blue {
+                            is_possible = false;
+                        }
                     }
-                }
-                if color.contains("green") {
-                    let count = color.replace("green", "").trim().parse::<u8>().unwrap();
-                    if count > green {
-                        is_possible = false;
+                    "green" => {
+                        if count > green {
+                            is_possible = false;
+                        }
                     }
+                    _ => (),
                 }
             }
         }
         if is_possible {
             sum += game;
         }
+    }
+
+    return sum;
+}
+
+pub fn part_2() -> u32 {
+    let buffer = std::fs::read_to_string("02-part1.txt").unwrap();
+
+    let mut sum: u32 = 0;
+
+    for line in buffer.lines() {
+        let (_, cube_str) = line.split_once(':').unwrap();
+
+        let mut red = 0;
+        let mut blue = 0;
+        let mut green = 0;
+
+        for set in cube_str.split(';') {
+            for color in set.split(',') {
+                let (count, color_name) = color
+                    .trim()
+                    .split_at(color.trim().find(|x: char| x.is_whitespace()).unwrap_or(0));
+
+                let count: u32 = count.parse().unwrap();
+
+                match color_name.trim() {
+                    "red" => {
+                        if count > red {
+                            red = count;
+                        }
+                    }
+                    "blue" => {
+                        if count > blue {
+                            blue = count;
+                        }
+                    }
+                    "green" => {
+                        if count > green {
+                            green = count;
+                        }
+                    }
+                    _ => (),
+                }
+            }
+        }
+        sum += red * green * blue;
     }
 
     return sum;
--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,5 @@ fn main() {
     println!("Day 01 - Part 01 - {}", day01::part_1());
     println!("Day 01 - Part 02 - {}", day01::part_2());
     println!("Day 02 - Part 01 - {}", day02::part_1());
+    println!("Day 02 - Part 02 - {}", day02::part_2());
 }
