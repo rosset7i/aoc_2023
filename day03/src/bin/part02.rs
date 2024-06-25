55f128a5dadfce0fd3b97b93767d9e990efac194
matheus.rossetti <matheus.rossetti@db1.com.br>
2024-06-25 15:50:58 -0300

    feat day03: part2

--- day03/src/bin/part02.rs
+++ day03/src/bin/part02.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashSet, fs};
 
 fn main() {
     let buffer = fs::read_to_string("input3.txt").unwrap();
@@ -7,7 +7,81 @@ fn main() {
 }
 
 fn process(input: &str) -> String {
-    todo!();
+    let mut parts: Vec<Part> = Vec::new();
+    let mut symbols: HashSet<(i16, i16)> = HashSet::new();
+
+    let mut current_digit: Option<Part> = None;
+    for (row, line) in input.lines().enumerate() {
+        for (col, char) in line.chars().enumerate() {
+            if char.is_ascii_digit() {
+                match current_digit {
+                    None => current_digit = Some(Part::from(row as i16, col as i16, char)),
+                    Some(ref mut current) => current.add_digit(char, row as i16, col as i16),
+                }
+            } else {
+                if let Some(current_digit) = current_digit.take() {
+                    parts.push(current_digit);
+                }
+                if char == '*' {
+                    symbols.insert((row as i16, col as i16));
+                }
+            }
+        }
+    }
+
+    let mut ratios = 0;
+    for gear in symbols {
+        let mut matches = Vec::new();
+        for part in &parts {
+            if part.points.contains(&gear) {
+                matches.push(part.value.clone());
+            }
+        }
+        if matches.len() == 2 {
+            ratios += matches[0].parse::<u32>().unwrap() * matches[1].parse::<u32>().unwrap();
+        }
+    }
+
+    return ratios.to_string();
+}
+
+struct Part {
+    value: String,
+    points: HashSet<(i16, i16)>,
+}
+
+impl Part {
+    fn from(row: i16, col: i16, ch: char) -> Self {
+        let adjacent_points = HashSet::from([
+            (row - 1, col - 1),
+            (row - 1, col),
+            (row - 1, col + 1),
+            (row, col + 1),
+            (row + 1, col + 1),
+            (row + 1, col),
+            (row + 1, col - 1),
+            (row, col - 1),
+        ]);
+
+        return Self {
+            value: String::from(ch),
+            points: adjacent_points,
+        };
+    }
+
+    fn add_digit(&mut self, digit: char, row: i16, col: i16) {
+        self.value.push(digit);
+        self.points.extend([
+            (row - 1, col - 1),
+            (row - 1, col),
+            (row - 1, col + 1),
+            (row, col + 1),
+            (row + 1, col + 1),
+            (row + 1, col),
+            (row + 1, col - 1),
+            (row, col - 1),
+        ]);
+    }
 }
 
 #[cfg(test)]
@@ -16,6 +90,19 @@ mod tests {
 
     #[test]
     fn it_works() {
-        todo!();
+        let result = process(
+            "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..",
+        );
+
+        assert_eq!(result, 467835.to_string());
     }
 }
